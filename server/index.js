b72115ad7901b1c8b5c1ba3ecef30cc9b98e34ca
INOPraIt <inoprait@gmail.com>
2024-10-29 16:12:51 +0300

    sendcommit

--- server/index.js
+++ server/index.js
@@ -10,6 +10,8 @@ const path = require('path');
 const PORT = process.env.PORT || 5000;
 const app = express();
 
+app.get("/", (req, res) => res.send("Express on Vercel"));
+
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 app.use(express.json());
 app.use(cookieParser());
